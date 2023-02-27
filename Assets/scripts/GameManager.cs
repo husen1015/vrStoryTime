1bb765d48d788ebcfc6c9536a87595317c2d3eca
husen1015 <husen.ab1015@gmail.com>
2023-02-27 19:08:17 +0200

    added a bit of roaming for the three butts in the field after they meet

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -8,11 +8,14 @@ public class GameManager : MonoBehaviour
 {
 
     public GameObject redButt;
+    public GameObject yellowButt;
     public GameObject Lion;
 
-    private bool reachedRedButt = false;
     private Animator animator;
-    private int Waiting_Time = 20;
+    private bool reachedRedButt = false;
+    private bool reachedYellowButt = false;
+    private int Waiting_Time = 2; // used for pausing the lion walk animation until the conversation is over
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,31 +33,26 @@ public class GameManager : MonoBehaviour
             reachedRedButt = true;
             redButt.SetActive(true);
             //waitforConvo(5);
-            StartCoroutine("waitTenSeconds");
+            StartCoroutine("waitXSeconds");
             
+        }
+
+        //make the yellow butterfly appear
+        if (!reachedYellowButt & Lion.transform.position.x < 110)
+        {
+            Debug.Log("yellow");
+            reachedYellowButt = true;
+            yellowButt.SetActive(true);
+            StartCoroutine("waitXSeconds");
+
         }
     }
 
-    //this function waits for 'time' seconds before continuing, it does so by simply being stuck in a loop. can make it more effeciently in the future
-    void waitforConvo(int time)
-    {
-        //curr time
-        DateTime start = DateTime.Now;
-        DateTime end = start;
 
-        //// while currTime - startTime < seconds wait
-        //while ((end - start).TotalSeconds < time)
-        //{
-        //    //wait
-        //    end = DateTime.Now;
-        //}
-        Debug.Log(start);
-
-    }
-    IEnumerator waitTenSeconds()
+    IEnumerator waitXSeconds()
     {
         yield return new WaitForSeconds(Waiting_Time);
-        // SET BOOL TO FALSE WHEN 5 SECONDS PASSED
+        // SET BOOL TO FALSE WHEN Waiting_Time in SECONDS PASSED
         animator.SetBool("wait", false);
     }
 
