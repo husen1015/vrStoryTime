debdc9b4106ac117e5b12bc3efbbf30352c78e70
Husen Abdallah <Husen.ab1015@gmail.com>
2023-03-03 19:08:10 +0200

    added anim- victoryDance

--- Assets/Chapter12.cs
+++ Assets/Chapter12.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// the sun hears the butterflies' story and banished the rain away, the weather becomes pleasant again 
+/// the sun hears the butterflies' story and banishes the rain away, the weather becomes pleasant again 
 /// </summary>
 public class Chapter12 : MonoBehaviour
 {
     public GameObject skyController;
     public GameObject rainController;
+    public GameObject butterflies;
+
+    private Animator butterflyAnimator;
 
 
     // Start is called before the first frame update
@@ -18,6 +21,8 @@ public class Chapter12 : MonoBehaviour
         //return weather back to sunny
         skyController.GetComponent<SkyboxController>().fadeToCloudyWeather = false;
         rainController.GetComponent<RainScript>().RainIntensity = 0;
+        butterflyAnimator = butterflies.GetComponent<Animator>();
+        butterflyAnimator.SetBool("shouldCelebrate", true);
     }
 
     // Update is called once per frame
